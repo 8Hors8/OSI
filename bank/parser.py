1be989a47af12c527a241be7b20d276a201fd01b
Hors-work <i.kovalenko.everest@gmail.com>
2025-12-11 15:08:48 +0500

    add def acquisition_data

--- bank/parser.py
+++ bank/parser.py
@@ -1,7 +1,11 @@
 import logging
+
 import openpyxl as op
+import re
 from openpyxl.workbook import Workbook
 from typing import Optional
+from openpyxl.utils.exceptions import InvalidFileException
+
 
 logger = logging.getLogger(__name__)
 
@@ -13,19 +17,48 @@ def load_bank_file(file_path: str)-> Optional[Workbook]:
     """
     try:
         # data_only=True — если нужен результат формул
-        return op.load_workbook(file_path, data_only=True)
+        book = op.load_workbook(file_path, data_only=True)
+        sheet = book.active
+        logger.info(f'Фйл с банка успешно загружен!')
+        return sheet
     except FileNotFoundError:
         logger.error(f'Ошибка: Файл не найден по пути: "{file_path}"')
         return None
     except PermissionError:
         logger.error(f'Ошибка: Нет доступа. Закройте файл "{file_path}"')
         return None
-    except op.utils.exceptions.InvalidFileException:
+    except InvalidFileException:
         logger.error(f'Ошибка: Файл "{file_path}" не является корректным Excel-файлом.')
         return None
     except Exception as e:
         logger.error(f'Непредвиденная ошибка при загрузке файла: {e}')
         return None
+
+
+def extract_apartment_number (apartment_data:str) -> Optional[int]:
+    pass
+
+
+def acquisition_data(sheet) -> Optional[dict[str, list[str]]]:
+    """
+    Получаем данные с листа эксель вид счета на который зачисляются средства, номера квартир дату платежа
+     и сумму платежа
+     :param sheet: Лист банковского фала
+     :return: dict с данными вида платежа и списком квартир
+    """
+    result = {}
+    max_row = sheet.max_row
+    print(f'max_row: {max_row}')
+    for row in range(2, max_row + 1):
+        print(f'row: {row}, значение;{sheet.cell(row=row, column=2).value}')
+        if sheet.cell(row=row, column=2).value is not None:
+            payment_type = sheet.cell(row=row, column=1).value
+            apartment_number = sheet.cell(row=row, column=2).value
+            sum_amount = sheet.cell(row=row, column=4).value
+            data_amount = sheet.cell(row=row, column=5).value
+            result[f'{apartment_number}']=[{'type': payment_type, 'sum': sum_amount, 'data': data_amount}]
+    return result
+
 
 
 if __name__ == '__main__':
@@ -35,6 +68,4 @@ if __name__ == '__main__':
             format="[%(asctime)s.%(msecs)03d] %(module)s:%(lineno)d %(levelname)7s - %(message)s"
         )
 
-    sheet = load_bank_file(r"D:\для теста оси\Новые ведомости\03.25.xlsx")
-    if sheet:
-        print(f"Открыт лист: {sheet.max_row}")
+
