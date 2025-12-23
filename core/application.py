d1733208b080fb60a00891ff62ab1bb3adc0ca7d
Hors-work <i.kovalenko.everest@gmail.com>
2025-12-23 18:02:16 +0500

    feat:The ManagerStatements class and methods for working with notifications have been added.

--- core/application.py
+++ core/application.py
@@ -5,6 +5,8 @@ application.py
 import logging
 
 from bank.manager_bank import ManagerBank
+from statement_processing.statements_manager import ManagerStatements
+from statement_processing.statement_schema import ApartmentsSchema
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +24,12 @@ class OSIApplication:
         self.statement = None
 
     def run(self):
+        self.statement = ManagerStatements(self.statement_path)
+        self.statement.load_statements()
+        apartment_numbers = self.statement.get_apartment_numbers(ApartmentsSchema)
         self.bank = ManagerBank(self.bank_path)
         self.bank.load_sheet()
-        self.bank.acquire_payments()
+        self.bank.acquire_payments(apartment_numbers)
 
 
 
@@ -35,7 +40,7 @@ if __name__ == '__main__':
             format="[%(asctime)s.%(msecs)03d] %(module)s:%(lineno)d %(levelname)7s - %(message)s"
         )
     bank_path1 = r'D:\googleDriver\ОСИ исходники\пробный вариант.xlsx'
-    statement_path1 = r'D:\googleDriver\ОСИ исходники\Ведомость на 2026год.xls'
+    statement_path1 = r'D:\googleDriver\ОСИ исходники\тест ведомости.xlsx'
     app = OSIApplication(bank_path1, statement_path1)
     app.run()
     print(app.bank.data)
